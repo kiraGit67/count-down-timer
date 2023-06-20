2b1660ae268c6d5f09531568779dd40feb51d2e4
kiraGit67 <kirsten@sais.de>
2023-06-20 15:57:34 +0200

    adds functional script

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//Variables for
+//Start Button
+const startButton = document.querySelector("#start-btn");
+
+//and count div
+const countElement = document.querySelector("#count");
+
+//and counter
+let counter = 0;
+
+startButton.addEventListener("click", function () {
+  counter = 10;
+  countElement.hidden = false;
+  startButton.hidden = true;
+
+  const intervalId = setInterval(function () {
+    counter--;
+    console.log(counter);
+    countElement.innerHTML = counter;
+
+    if (counter === -1) {
+      clearInterval(intervalId);
+      countElement.hidden = true;
+      startButton.hidden = false;
+      counter = 10;
+      countElement.innerHTML = counter;
+    }
+  }, 1000);
+});
